961c44cb33166b39eb1b19adc3cb12d5d72aed42
visualstudio <visualstudio@122-PC22>
2019-04-10 10:49:37 -0400

    Section C

--- C0732149-Assignment4/Program.cs
+++ C0732149-Assignment4/Program.cs
@@ -25,16 +25,37 @@ namespace C0732149_Assignment4
             using (StreamReader file = new StreamReader("U:/Users/732149/beowulf.txt"))
             {
                 int counter = 0;
-                string ln;
 
-                while ((ln = file.ReadLine()) != null)
+                int number = 0;
+
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+
+                {
+
+                    if (line.Contains("Sea") && line.Contains("Fare") || line.Contains("sea") && line.Contains("fare"))
+
                     {
-                    Console.WriteLine(ln);
-                    Beowulf.Add(ln);
+
+                        int x = counter - 1;
+
+                        number++;
+
                     }
+
+                    counter++;
+
+                }
+
+                Console.WriteLine($"The number of lines that contains *Sea* and *Fare* are {number}");
+
+
+
                 file.Close();
                 counter = File.ReadLines("").Count();
-                Console.WriteLine("Number of Lines are " +counter);
+                Console.WriteLine("Number of Lines in the Beowulf File Are " + counter);
+                
             }
         }
         public int FindNumberOfBlankSpaces(string line)
